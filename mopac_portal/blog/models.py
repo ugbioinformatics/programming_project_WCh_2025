296da62a29fd273c99426407894899928cb641d8
MsBasis <matikgawin@gmail.com>
2025-03-25 14:21:04 +0100

    podpisanie/wytlumaczenie czym sa modele

--- mopac_portal/blog/models.py
+++ mopac_portal/blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+#tworzenie obiektow("zmiennych nielokalnych"), ktore sa tworzone w bazie danych do wykorzystania w funkcjach do np. obliczen oraz wyswietlania ich w htmlu
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(User,on_delete=models.CASCADE,blank = True,null = True)
