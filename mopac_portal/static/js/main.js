7fd20764c28f4350463e5d17526939a0b4d56f99
kkgrz13 <k.kozlowski684@gmail.com>
2025-04-15 16:28:38 +0200

    tak

--- mopac_portal/static/js/main.js
+++ mopac_portal/static/js/main.js
@@ -13,17 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         stage.handleResize();
     }, false );
     
-    
     // Code for example: interactive/simple-viewer
-    
-    function addElement (el) {
-      Object.assign(el.style, {
-        position: "absolute",
-        zIndex: 10
-      })
-      stage.viewer.container.appendChild(el)
-    }
-    
     function createElement (name, properties, style) {
       var el = document.createElement(name)
       Object.assign(el, properties)
@@ -41,52 +31,19 @@ document.addEventListener("DOMContentLoaded", function () {
       return select
     }
     
-    function createFileButton (label, properties, style) {
-      var input = createElement("input", Object.assign({
-        type: "file"
-      }, properties), { display: "none" })
-      addElement(input)
-      var button = createElement("input", {
-        value: label,
-        type: "button",
-        onclick: function () { input.click() }
-      }, style)
-      return button
-    }
-    
     function loadStructure (input) {
       stage.removeAllComponents()
       return stage.loadFile(input).then(function (o) {
         o.autoView()
-        o.addRepresentation('surface', {
-          sele: "polymer",
-          name: "polymer"
-        })
         o.addRepresentation("ball+stick", {
           name: "ligand",
-          visible: ligandCheckbox.checked,
+          visible: true,
           sele: "not ( polymer or water or ion )"
-        })
-        o.addRepresentation("spacefill", {
-          name: "waterIon",
-          visible: waterIonCheckbox.checked,
-          sele: "water or ion",
-          scale: 0.25
         })
       })
     }
     
-    var loadStructureButton = createFileButton("load structure", {
-      accept: ".pdb,.cif,.ent,.gz,.mol2",
-      onchange: function (e) {
-        if (e.target.files[ 0 ]) {
-          loadStructure(e.target.files[ 0 ])
-        }
-      }
-    })
-    document.getElementById('buttons').appendChild(loadStructureButton)
-    
-    var polymerSelect = createSelect([
+    var backgroundSelect = createSelect([
       [ "white", "white" ],
       [ "black", "black" ],
       [ "transparent", "transparent" ],
@@ -104,55 +61,29 @@ document.addEventListener("DOMContentLoaded", function () {
         })
       }
     })
-    document.getElementById('buttons').appendChild(polymerSelect)
-    
-    var ligandCheckbox = createElement("input", {
-      type: "checkbox",
-      checked: true,
-      onchange: function (e) {
-        stage.getRepresentationsByName("ligand")
-          .setVisibility(e.target.checked)
-      }
-    })
-    addElement(ligandCheckbox)
-    addElement(createElement("span", {
-      innerText: "ligand"
-    }))
-    
-    var waterIonCheckbox = createElement("input", {
-      type: "checkbox",
-      checked: false,
-      onchange: function (e) {
-        stage.getRepresentationsByName("waterIon")
-          .setVisibility(e.target.checked)
-      }
-    }, { top: "84px", left: "12px" })
-    addElement(waterIonCheckbox)
-    addElement(createElement("span", {
-      innerText: "water+ion"
-    }, { top: "84px", left: "32px" }))
+    document.getElementById('buttons1').appendChild(backgroundSelect)
     
     x = false
-    
     var spinButton = createElement("input", {
       type: "button",
       id: "toggleSpin",
-      value: "Spin",
+      value: "Spin On",
       onclick: function () {
         if (x){
         stage.setSpin(false)
         x=false
+        spinButton.value = "Spin On"
         }
         else{
         stage.spinAnimation = stage.animationControls.spin([ 0, 1, 0 ], 0.05)
         stage.setSpin(true)
         x=true
+        spinButton.value = "Spin Off"
         }
       }
     }, { top: "108px", left: "12px" })
-    document.getElementById('buttons').appendChild(spinButton)
-    postid = document.getElementById("postid").textContent
-    loadStructure("/media/"+postid+"/start.mol2")
+    document.getElementById('buttons1').appendChild(spinButton)
+
     
     var centerButton = createElement("input", {
       type: "button",
@@ -161,10 +92,8 @@ document.addEventListener("DOMContentLoaded", function () {
       onclick: function () {
         stage.autoView(1000)
       }
-    }, { top: "108px", left: "12px" })
-    document.getElementById('buttons').appendChild(centerButton)
-    postid = document.getElementById("postid").textContent
-    loadStructure("/media/"+postid+"/start.mol2")
+    })
+    document.getElementById('buttons1').appendChild(centerButton)
     
     var Labels = createElement("input", {
       type: "button",
@@ -173,11 +102,11 @@ document.addEventListener("DOMContentLoaded", function () {
       onclick: function () {
         stage.La
       }
-    }, { top: "108px", left: "12px" })
-    document.getElementById('buttons').appendChild(Labels)
+    })
+    document.getElementById('buttons1').appendChild(Labels)
+
     postid = document.getElementById("postid").textContent
     loadStructure("/media/"+postid+"/start.mol2")
-    
     });
     
     
@@ -198,17 +127,7 @@ document.addEventListener("DOMContentLoaded", function () {
           stage.handleResize();
       }, false );
       
-      
       // Code for example: interactive/simple-viewer
-      
-      function addElement (el) {
-        Object.assign(el.style, {
-          position: "absolute",
-          zIndex: 10
-        })
-        stage.viewer.container.appendChild(el)
-      }
-      
       function createElement (name, properties, style) {
         var el = document.createElement(name)
         Object.assign(el, properties)
@@ -225,53 +144,21 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         return select
       }
-      
-      function createFileButton (label, properties, style) {
-        var input = createElement("input", Object.assign({
-          type: "file"
-        }, properties), { display: "none" })
-        addElement(input)
-        var button = createElement("input", {
-          value: label,
-          type: "button",
-          onclick: function () { input.click() }
-        }, style)
-        return button
-      }
+    
       
       function loadStructure (input) {
         stage.removeAllComponents()
         return stage.loadFile(input).then(function (o) {
           o.autoView()
-          o.addRepresentation('surface', {
-            sele: "polymer",
-            name: "polymer"
-          })
           o.addRepresentation("ball+stick", {
             name: "ligand",
-            visible: ligandCheckbox.checked,
+            visible: true,
             sele: "not ( polymer or water or ion )"
-          })
-          o.addRepresentation("spacefill", {
-            name: "waterIon",
-            visible: waterIonCheckbox.checked,
-            sele: "water or ion",
-            scale: 0.25
           })
         })
       }
       
-      var loadStructureButton = createFileButton("load structure", {
-        accept: ".pdb,.cif,.ent,.gz,.mol2",
-        onchange: function (e) {
-          if (e.target.files[ 0 ]) {
-            loadStructure(e.target.files[ 0 ])
-          }
-        }
-      })
-      document.getElementById('buttons2').appendChild(loadStructureButton)
-      
-      var polymerSelect = createSelect([
+      var backgroundSelect = createSelect([
         [ "white", "white" ],
         [ "black", "black" ],
         [ "transparent", "transparent" ],
@@ -289,36 +176,9 @@ document.addEventListener("DOMContentLoaded", function () {
           })
         }
       })
-      document.getElementById('buttons2').appendChild(polymerSelect)
-      
-      var ligandCheckbox = createElement("input", {
-        type: "checkbox",
-        checked: true,
-        onchange: function (e) {
-          stage.getRepresentationsByName("ligand")
-            .setVisibility(e.target.checked)
-        }
-      })
-      addElement(ligandCheckbox)
-      addElement(createElement("span", {
-        innerText: "ligand"
-      }))
-      
-      var waterIonCheckbox = createElement("input", {
-        type: "checkbox",
-        checked: false,
-        onchange: function (e) {
-          stage.getRepresentationsByName("waterIon")
-            .setVisibility(e.target.checked)
-        }
-      }, { top: "84px", left: "12px" })
-      addElement(waterIonCheckbox)
-      addElement(createElement("span", {
-        innerText: "water+ion"
-      }, { top: "84px", left: "32px" }))
-      
+      document.getElementById('buttons2').appendChild(backgroundSelect)
+  
       x = false
-      
       var spinButton = createElement("input", {
         type: "button",
         id: "toggleSpin",
@@ -334,10 +194,8 @@ document.addEventListener("DOMContentLoaded", function () {
           x=true
           }
         }
-      }, { top: "108px", left: "12px" })
+      })
       document.getElementById('buttons2').appendChild(spinButton)
-      postid = document.getElementById("postid").textContent
-      loadStructure("/media/"+postid+"/start.mol2")
       
       var centerButton = createElement("input", {
         type: "button",
@@ -346,30 +204,9 @@ document.addEventListener("DOMContentLoaded", function () {
         onclick: function () {
           stage.autoView(1000)
         }
-      }, { top: "108px", left: "12px" })
+      })
       document.getElementById('buttons2').appendChild(centerButton)
-      postid = document.getElementById("postid").textContent
-      loadStructure("/media/"+postid+"/molecule.mol2")
       
-      y = false
-      var bgButton = createElement("input", {
-        type: "button",
-        id: "toggleBg",
-        value: "Background",
-        onclick: function () {
-          if (y){
-          stage.setParameters({ backgroundColor: "black" })
-          y=false
-          }
-          else{
-          stage.setParameters({ backgroundColor: "white" })
-          y=true
-          }
-        }
-      }, { top: "108px", left: "12px" })
-      document.getElementById('buttons2').appendChild(bgButton)
       postid = document.getElementById("postid").textContent
       loadStructure("/media/"+postid+"/start.mol2")
-         
-      
       });
